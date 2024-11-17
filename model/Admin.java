823bbadbefc9a840e2c22dc5d6317d1d83c1c484
biancasio <161092766+biancasio@users.noreply.github.com>
2024-11-16 22:08:18 -0300

    upload da pasta model do exchange admin
    upload da pasta model do exchange admin

---
+++ model/Admin.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Aline Rocha
+ */
+public class Admin {
+    private String nome, cpf, senha;
+
+    public Admin(String nome, String cpf, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public Admin() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+    
+}
